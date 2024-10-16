6cfe925deba96e423783964ad99abd8661638121
joaovlummertz <joaolummertz1@gmail.com>
2024-10-16 19:13:41 -0300

    Created Header component

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import banner from "../../../public/banner.png"
+
+export default function Header() {
+    return (
+        <header className="relative w-full h-[90vh]">
+            <Image
+                className="dark:invert opacity"
+                src={banner}
+                alt="Banner"
+                objectFit="cover"
+                objectPosition="bottom"
+                layout="fill"
+                priority
+            />
+
+            <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+
+            <div className="absolute inset-0 items-end justify-end z-20 pb-24 pr-32 flex flex-col">
+                <h1 className="text-white text-9xl font-cinematografica font-normal tracking-widest">CineIDE</h1>
+                <h1 className="text-white text-3xl font-light">Clássicos do terror, onde o pavor nunca sai de cena</h1>
+            </div>
+        </header>
+      );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,13 @@
-import Image from "next/image";
-import banner from "../../public/banner.png"
+
 import background from "../../public/background.png"
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
-    <body className="w-full h-full bg-cover bg-center" style={{
+    <body className="w-full h-full bg-cover bg-center bg-no-repeat" style={{
       backgroundImage: `url(${background.src})`
     }}>
-      <header className="relative w-full h-[90vh]">
-        <Image
-            className="dark:invert opacity"
-            src={banner}
-            alt="Banner"
-            objectFit="cover"
-            objectPosition="bottom"
-            layout="fill"
-            priority
-          />
-
-          <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
-
-          <div className="absolute inset-0 items-end justify-end z-20 pb-24 pr-32 flex flex-col">
-              <h1 className="text-white text-9xl font-cinematografica font-normal tracking-widest">CineIDE</h1>
-              <h1 className="text-white text-3xl font-light">Clássicos do terror, onde o pavor nunca sai de cena</h1>
-          </div>
-      </header>
+      <Header />
 
       <section id="movies" className="min-h-[80vh]">
         <h2 className="text-white">Filmes em cartaz</h2>
