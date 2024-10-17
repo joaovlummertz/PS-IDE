5ae509747e48daeaf7bee07854ff96b37b04b452
bsmengue <bruno.mengue@hotmail.com>
2024-10-17 11:32:39 -0300

    adicionando catalago

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -12,6 +13,8 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,211 @@
+
+"use client";
+
 import Image from "next/image";
+import { relative } from "path";
+import { Bevan } from 'next/font/google'
+
+const bevan = Bevan({ weight: '400',subsets: ['latin'] })
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <div className="filmes">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <h1>FILMES EM CARTAZ</h1>
+
+      <div className="container">
+
+        <div className = "image">
+          <img
+            src="/Filme1.png"        
+            alt="Poster de Sexta-Feira 13"
+          />
+          </div>
+        
+        <div className = "image">
+          <img
+            src="/Filme3.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+        <div className = "image">
+          <img
+            src="/Filme4.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+        <div className = "image">
+          <img
+            src="/Filme5.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+         <div className = "image">
+          <img
+            src="/Filme6.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+
+        <div className = "image">
+          <img
+            src="/Filme7.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+
+        <div className = "image">
+          <img
+            src="/Filme8.png"        
+            alt="Poster de O Chamado"
+          />
+          </div>
+
+        <div className = "image">
+          <img
+            src="/Filme9.png"        
+            alt="Poster de Hellraiser"
+          />
+          </div>
+
+        <div className = "image">
+          <img
+            src="/Filme10.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+        <div className = "image">
+          <img
+            src="/Filme11.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+        <div className = "image">
+          <img
+            src="/Filme12.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+        <div className = "image">
+          <img
+            src="/Filme13.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+
+        <div className = "image">
+          <img
+            src="/Filme14.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+
+        <div className = "image">
+          <img
+            src="/Filme15.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+
+        <div className = "image">
+          <img
+            src="/Filme16.png"        
+            alt="Poster de Filme"
+          />
+          </div>
+
+
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      
+
+      <style jsx>{`
+       h1 {
+        position: relative;
+        font-family: bevan;
+        font-size: 75px;
+        font-weight: 400;
+        line-height: 55.33px;
+        text-align: left;
+        width: auto;
+        height: 55px;
+        gap: 0px;
+        left: 100px;
+        top: 50px;
+        color: white
+       }
+
+
+        h2 {
+
+        font-family: bevan;
+        font-size: 25px;
+        font-weight: 400;
+        line-height: 55.33px;
+        text-align: left;
+        width: auto;
+        height: 55px;
+        gap: 0px;
+          text-align: center;
+       }
+      
+       .container
+          {
+          position: relative;
+          justify-content: center;
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+          gap: 100px;
+          width: 90%;
+          top: 150px;
+          margin: 0 auto;
+          }
+        
+        .image{
+          width: 120%;
+          height: auto;
+          display: block;
+          text-align: center;
+        }
+        
+        .filmes {
+          background-image: url("/background.png");
+          background-attachment: scroll;
+          background-repeat: repeat-y;
+          }
+
+        
+          
+        .image img:hover {
+          transform: scale(1.2);
+          transition:transform 0.3s ease-in-out;
+
+      }
+
+        
+
+      `}
+      
+      </style>
+
+        </div>
+
+
+
+
+
+          );
+    
 }
