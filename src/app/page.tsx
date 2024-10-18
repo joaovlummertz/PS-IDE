3c2b1c0d57d88fa7b8eebf75d5db999cd0e1e162
joaovlummertz <joaolummertz1@gmail.com>
2024-10-18 16:59:36 -0300

    Footer component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 import Header from "./_components/Header";
 import Movies from "./_components/Movies";
 import Killers from "./_components/Killers";
+import Footer from "./_components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
 
-      <section id="movies" className="min-h-[80vh] w-[80%] mx-auto py-16 scroll-mt-[150px]">
-           <Movies />
-      </section>
+      <Movies />
 
       <Killers />
 
       <section id="tickets" className="min-h-[80vh]">
        <h2 className="text-white">Comprar ingresso</h2>
       </section>
+
+      <Footer />
     </>
   );
     
